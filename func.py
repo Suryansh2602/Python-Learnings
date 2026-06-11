d966c2d702455345341653581b8c381bf7384fbd
Suryansh2602 <suryanshpushpkar183@gmail.com>
2026-06-11 19:13:04 +0530

    Functions

---
+++ func.py
@@ -0,0 +1,6 @@
+def greet():
+    print("Hello world")
+    pass
+
+
+greet()
---
+++ func2.py
@@ -0,0 +1,9 @@
+def check_weather():
+    temp = 20
+    if(temp>20):
+        print("It's hot")
+    else:
+        print("It's cold")
+
+
+check_weather() 
